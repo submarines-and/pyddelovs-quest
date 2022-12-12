2911c37e0cafe393a3e034346a0e448d1ebe3c3c
submarines-and <johannes.sorensen@ingka.ikea.com>
2022-12-12 07:49:17 +0100

    clang format

--- src/map.cpp
+++ src/map.cpp
@@ -31,55 +31,46 @@ int levelOne[20][25] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
+Map::Map() {
+  dirt = TextureManager::loadTexture("assets/dirt.png");
+  grass = TextureManager::loadTexture("assets/grass.png");
+  water = TextureManager::loadTexture("assets/water.png");
 
-Map::Map()
-{
-    dirt = TextureManager::loadTexture("assets/dirt.png");
-    grass = TextureManager::loadTexture("assets/grass.png");
-    water = TextureManager::loadTexture("assets/water.png");
+  load(levelOne);
 
-    load(levelOne);
-
-    src.x = src.y = dest.x = dest.y = 0;
-    src.w = src.h = dest.w = dest.h = 32;
+  src.x = src.y = dest.x = dest.y = 0;
+  src.w = src.h = dest.w = dest.h = 32;
 }
 
-void Map::load(int array[20][25])
-{
-    for (int row = 0; row < 20; row++)
-    {
-        for (int column = 0; column < 25; column++)
-        {
-            currentMap[row][column] = array[row][column];
-        }
+void Map::load(int array[20][25]) {
+  for (int row = 0; row < 20; row++) {
+    for (int column = 0; column < 25; column++) {
+      currentMap[row][column] = array[row][column];
     }
+  }
 }
 
-void Map::render()
-{
-    for (int row = 0; row < 20; row++)
-    {
-        for (int column = 0; column < 25; column++)
-        {
-            dest.x = column * 32;
-            dest.y = row * 32;
+void Map::render() {
+  for (int row = 0; row < 20; row++) {
+    for (int column = 0; column < 25; column++) {
+      dest.x = column * 32;
+      dest.y = row * 32;
 
-            switch (currentMap[row][column])
-            {
+      switch (currentMap[row][column]) {
 
-            default:
-            case WATER:
-                TextureManager::render(water, src, dest);
-                break;
+      default:
+      case WATER:
+        TextureManager::render(water, src, dest);
+        break;
 
-            case GRASS:
-                TextureManager::render(grass, src, dest);
-                break;
+      case GRASS:
+        TextureManager::render(grass, src, dest);
+        break;
 
-            case DIRT:
-                TextureManager::render(dirt, src, dest);
-                break;
-            }
-        }
+      case DIRT:
+        TextureManager::render(dirt, src, dest);
+        break;
+      }
     }
+  }
 }
--- src/texture-manager.h
+++ src/texture-manager.h
@@ -1,9 +1,8 @@
 #pragma once
 #include "game.h"
 
-class TextureManager
-{
+class TextureManager {
 public:
-    static SDL_Texture *loadTexture(const char *filename);
-    static void render(SDL_Texture* texture, SDL_Rect src, SDL_Rect dest);
+  static SDL_Texture* loadTexture(const char* filename);
+  static void render(SDL_Texture* texture, SDL_Rect src, SDL_Rect dest);
 };
