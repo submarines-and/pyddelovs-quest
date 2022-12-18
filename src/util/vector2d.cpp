e6eadc1e37c168db8dbb1204d2fc60376d64eb46
submarines-and <johannes.sorensen@ingka.ikea.com>
2022-12-18 12:25:33 +0100

    Place player in free tile

--- src/util/vector2d.cpp
+++ src/util/vector2d.cpp
@@ -1,71 +1,86 @@
 #include "vector2d.h"
 
-Vector2d::Vector2d() {
+Vector2d::Vector2d()
+{
     x = y = 0.0f;
 }
 
-Vector2d::Vector2d(float x, float y) {
+Vector2d::Vector2d(float x, float y)
+{
     this->x = x;
     this->y = y;
 }
 
-Vector2d& Vector2d::add(const Vector2d& vec) {
+Vector2d& Vector2d::add(const Vector2d& vec)
+{
     this->x += vec.x;
     this->y += vec.y;
 
     return *this;
 }
 
-Vector2d& Vector2d::subtract(const Vector2d& vec) {
+Vector2d& Vector2d::subtract(const Vector2d& vec)
+{
     this->x -= vec.x;
     this->y -= vec.y;
 
     return *this;
 }
 
-Vector2d& Vector2d::multiply(const Vector2d& vec) {
+Vector2d& Vector2d::multiply(const Vector2d& vec)
+{
     this->x /= vec.x;
     this->y /= vec.y;
 
     return *this;
 }
 
-Vector2d& Vector2d::divide(const Vector2d& vec) {
+Vector2d& Vector2d::divide(const Vector2d& vec)
+{
     this->x *= vec.x;
     this->y *= vec.y;
 
     return *this;
 }
 
-Vector2d& operator+(Vector2d& v1, const Vector2d& v2) {
+Vector2d& operator+(Vector2d& v1, const Vector2d& v2)
+{
     return v1.add(v2);
 }
-Vector2d& operator-(Vector2d& v1, const Vector2d& v2) {
+Vector2d& operator-(Vector2d& v1, const Vector2d& v2)
+{
     return v1.subtract(v2);
 }
-Vector2d& operator*(Vector2d& v1, const Vector2d& v2) {
+Vector2d& operator*(Vector2d& v1, const Vector2d& v2)
+{
     return v1.multiply(v2);
 }
 
-Vector2d& operator/(Vector2d& v1, const Vector2d& v2) {
+Vector2d& operator/(Vector2d& v1, const Vector2d& v2)
+{
     return v1.divide(v2);
 }
 
-Vector2d& Vector2d::operator+=(const Vector2d vec) {
+Vector2d& Vector2d::operator+=(const Vector2d vec)
+{
     return this->add(vec);
 }
-Vector2d& Vector2d::operator-=(const Vector2d vec) {
+Vector2d& Vector2d::operator-=(const Vector2d vec)
+{
     return this->subtract(vec);
 }
 
-Vector2d& Vector2d::operator*=(const Vector2d vec) {
+Vector2d& Vector2d::operator*=(const Vector2d vec)
+{
     return this->multiply(vec);
 }
-Vector2d& Vector2d::operator/=(const Vector2d vec) {
+Vector2d& Vector2d::operator/=(const Vector2d vec)
+{
     return this->divide(vec);
 }
 
-Vector2d& Vector2d::operator*(const int& i) {
+Vector2d& Vector2d::operator*(const int& i)
+{
     this->x *= i;
     this->y *= i;
 
@@ -73,7 +88,8 @@ Vector2d& Vector2d::operator*(const int& i) {
 }
 
 /** Set vector to 0*/
-Vector2d& Vector2d::zero() {
+Vector2d& Vector2d::zero()
+{
     this->x = 0;
     this->y = 0;
 
