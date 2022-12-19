04772cf4fbc0c08978081daf007e75e1d41c0300
submarines-and <johannes.sorensen@ingka.ikea.com>
2022-12-19 19:34:44 +0100

    Krabban Klo is now chasing pyddelov

---
+++ src/systems/ai-system.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "components/transform.h"
+#include "ecs/system.h"
+#include "global.h"
+#include "util/vector2d.h"
+
+class AISystem : public System {
+
+public:
+    void update(Vector2d playerPosition)
+    {
+
+        for (auto e : entities) {
+            auto& transform = global.ecs->getComponent<Transform>(e);
+
+            transform.direction.x = playerPosition.x > transform.position.x ? 1 : -1;
+            transform.direction.y = playerPosition.y > transform.position.y ? 1 : -1;
+        }
+    }
+};
