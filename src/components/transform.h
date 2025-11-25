e0bda5593a42825dc4d92890d627dee19e760d39
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 07:11:03 +0100

    Fixed collision in new ECS

--- src/components/transform.h
+++ src/components/transform.h
@@ -9,4 +9,12 @@ struct Transform {
     int width = 32;
 
     int speed = 0;
+
+    static bool isOverlapping(Transform a, Transform b)
+    {
+        return a.position.x + a.width >= b.position.x &&
+               b.position.x + b.width >= a.position.x &&
+               a.position.y + a.height >= b.position.y &&
+               b.position.y + b.height >= a.position.y;
+    }
 };
--- src/systems/collision-system.h
+++ src/systems/collision-system.h
@@ -3,38 +3,22 @@
 #include "ecs/component-manager.h"
 #include "ecs/system.h"
 #include "util/vector2d.h"
-#include "util/quad-tree.h"
 #include "global.h"
 
 class CollisionSystem : public System {
-private:
-    QuadTree tree;
-
 public:
-    void update(Entity player, Vector2d playerPositionBeforeUpdates)
+    void update(Entity player, Transform playerTransformBeforeUpdates)
     {
         auto& playerTransform = global.ecs->getComponent<Transform>(player);
-
-        SDL_Rect a{
-            .w = playerTransform.width,
-            .h = playerTransform.height,
-            .x = (int)playerTransform.position.x,
-            .y = (int)playerTransform.position.y,
-        };
 
         for (auto e : entities) {
             auto& transform = global.ecs->getComponent<Transform>(e);
 
-            SDL_Rect b{
-                .w = transform.width,
-                .h = transform.height,
-                .x = (int)transform.position.x,
-                .y = (int)transform.position.y,
-            };
-
-            if (SDL_HasIntersection(&a, &b)) {
+            if (Transform::isOverlapping(playerTransform, transform)) {
                 // global.sound.playSoundEffect("sound/character/bounce.wav");
-                playerTransform.position = playerPositionBeforeUpdates;
+
+                playerTransform.position.x -= playerTransform.velocity.x * playerTransform.speed;
+                playerTransform.position.y -= playerTransform.velocity.y * playerTransform.speed;
                 break;
             }
         }
