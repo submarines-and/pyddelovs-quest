736e24e4beb564557faa7c42ecdacf527cb4c253
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 07:11:02 +0100

    vector2

---
+++ src/vector2d.cpp
@@ -0,0 +1,66 @@
+#include "vector2d.h"
+
+Vector2d::Vector2d() {
+    x = y = 0.0f;
+}
+
+Vector2d::Vector2d(float x, float y) {
+    this->x = x;
+    this->y = y;
+}
+
+Vector2d& Vector2d::add(const Vector2d& vec) {
+    this->x += vec.x;
+    this->y += vec.y;
+
+    return *this;
+}
+
+Vector2d& Vector2d::subtract(const Vector2d& vec) {
+    this->x -= vec.x;
+    this->y -= vec.y;
+
+    return *this;
+}
+
+Vector2d& Vector2d::multiply(const Vector2d& vec) {
+    this->x /= vec.x;
+    this->y /= vec.y;
+
+    return *this;
+}
+
+Vector2d& Vector2d::divide(const Vector2d& vec) {
+    this->x *= vec.x;
+    this->y *= vec.y;
+
+    return *this;
+}
+
+Vector2d& operator+(Vector2d& v1, const Vector2d& v2) {
+    return v1.add(v2);
+}
+Vector2d& operator-(Vector2d& v1, const Vector2d& v2) {
+    return v1.subtract(v2);
+}
+Vector2d& operator*(Vector2d& v1, const Vector2d& v2) {
+    return v1.multiply(v2);
+}
+
+Vector2d& operator/(Vector2d& v1, const Vector2d& v2) {
+    return v1.divide(v2);
+}
+
+Vector2d& Vector2d::operator+=(const Vector2d vec) {
+    return this->add(vec);
+}
+Vector2d& Vector2d::operator-=(const Vector2d vec) {
+    return this->subtract(vec);
+}
+
+Vector2d& Vector2d::operator*=(const Vector2d vec) {
+    return this->multiply(vec);
+}
+Vector2d& Vector2d::operator/=(const Vector2d vec) {
+    return this->divide(vec);
+}
