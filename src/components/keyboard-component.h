92e9ffbc3f389c62340454dc852bb0584de294f4
submarines-and <cached_lifters2j@icloud.com>
2025-11-25 07:11:02 +0100

    Added support for multiple animations.

--- src/components/keyboard-component.h
+++ src/components/keyboard-component.h
@@ -1,38 +1,48 @@
 #pragma once
 #include "../ecs.h"
-#include "transform-component.h"
 #include "../game.h"
+#include "transform-component.h"
+#include "sprite-component.h"
 
 class KeyboardComponent : public Component {
 public:
     TransformComponent* transform;
+    SpriteComponent* sprite;
 
-    void init() override {
+    void init() override
+    {
         transform = &entity->getComponent<TransformComponent>();
+        sprite = &entity->getComponent<SpriteComponent>();
     }
 
     /** Check for keypress */
-    void update() override {
+    void update() override
+    {
         if (Game::event.type == SDL_KEYDOWN) {
             switch (Game::event.key.keysym.sym) {
             case SDLK_w:
             case SDLK_UP:
                 transform->velocity.y = -1;
+                sprite->playAnimation("Walk");
                 break;
 
             case SDLK_s:
             case SDLK_DOWN:
                 transform->velocity.y = 1;
+                sprite->playAnimation("Walk");
                 break;
 
             case SDLK_a:
             case SDLK_LEFT:
                 transform->velocity.x = -1;
+                sprite->spriteFlipped = SDL_FLIP_HORIZONTAL;
+                sprite->playAnimation("Walk");
                 break;
 
             case SDLK_d:
             case SDLK_RIGHT:
                 transform->velocity.x = 1;
+                sprite->playAnimation("Walk");
                 break;
 
             default:
@@ -47,6 +57,8 @@ public:
             case SDLK_s:
             case SDLK_DOWN:
                 transform->velocity.y = 0;
+                sprite->spriteFlipped = SDL_FLIP_NONE;
+                sprite->playAnimation("Idle");
                 break;
 
             case SDLK_a:
@@ -55,6 +67,8 @@ public:
             case SDLK_d:
             case SDLK_RIGHT:
                 transform->velocity.x = 0;
+                sprite->spriteFlipped = SDL_FLIP_NONE;
+                sprite->playAnimation("Idle");
                 break;
 
             default:
